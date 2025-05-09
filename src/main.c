b6719c520d13c32fa2d7cfbf76d802fca93e78a2
PabloFC <pablofc_68@hotmail.com>
2025-05-09 10:39:01 +0200

    proyect structure added

---
+++ src/main.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 10:37:35 by pafuente          #+#    #+#             */
+/*   Updated: 2025/05/09 10:37:36 by pafuente         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+/*Estructura
+
+
+so_long/
+├── Makefile
+├── lib/
+│   └── MLX42/
+├── include/
+│   └── so_long.h
+├── src/
+│   ├── main.c
+│   ├── init.c
+│   ├── map.c
+│   ├── render.c
+│   ├── game.c
+│   └── utils.c
+├── assets/
+│   ├── wall.png
+│   ├── floor.png
+│   ├── player.png
+│   ├── collect.png
+│   └── exit.png
+├── maps/
+│   └── map.ber
+└── libft/ 
+*/
