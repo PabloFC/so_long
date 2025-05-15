237e8b75dfa2274560332e63179c2dffdc6acad7
PabloFC <pablofc_68@hotmail.com>
2025-05-15 12:10:09 +0200

    validate_map function

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 14:09:34 by pafuente          #+#    #+#             */
-/*   Updated: 2025/05/15 11:22:25 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/05/15 12:04:05 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,6 @@ typedef struct s_game
 void	init_game(t_game *game);
 void	error_exit(const char *msg);
 char	**read_map(const char *file, t_game *game);
+bool	validate_map(t_game *game);
 
 #endif
--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 12:35:37 by pafuente          #+#    #+#             */
-/*   Updated: 2025/05/15 11:24:20 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/05/15 12:01:49 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,53 @@ char	**read_map(const char *file, t_game *game)
 	map = ft_split(joined, '\n');
 	free(joined);
 	return (map);
+}
+
+static bool	is_valid_char(char c)
+{
+	return (c == '0' || c == '1' || c == 'P' || c == 'C' || c == 'E');
+}
+
+bool	validate_map(t_game *game)
+{
+	int		y;
+	int		x;
+	int		player = 0;
+	int		collect = 0;
+	int		exit = 0;
+
+	y = 0;
+	while (y < game->height)
+	{
+		if ((int)ft_strlen(game->map[y]) != game->width)
+			return (false); // No es rectangular
+
+		x = 0;
+		while (x < game->width)
+		{
+			char tile = game->map[y][x];
+			if (!is_valid_char(tile))
+				return (false);
+
+			if ((y == 0 || y == game->height - 1 || x == 0 || x == game->width - 1) && tile != '1')
+				return (false); // Bordes no cerrados
+
+			if (tile == 'P')
+			{
+				player++;
+				game->player_x = x;
+				game->player_y = y;
+			}
+			else if (tile == 'C')
+				collect++;
+			else if (tile == 'E')
+				exit++;
+			x++;
+		}
+		y++;
+	}
+	if (player != 1 || collect < 1 || exit < 1)
+		return (false);
+	game->collectibles = collect;
+	return (true);
 }
